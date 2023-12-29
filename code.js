b45fc331c8ba06a349e683c69e68dbef8fd1facb
shadowm1k1 <bajrektarevic01@gmail.com>
2023-12-29 23:51:19 +0100

    12
    4131

--- code.js
+++ code.js
@@ -1,12 +1,55 @@
-function Button_Click()
-{ 
-  var Username = document.getElementById('Username').value;
-  var Password = document.getElementById('Password').value;
+var counter = 0;
 
-  if(Username == "Ece" && Password == "isTheBest")
-  {
-    open('surprise.html');
-  }
+
+function addHeart()
+{
+  counter++;
+  var spaceToMove = 100
+
+  const triangle = document.createElement("div");
   
+  triangle.style.width = "100px";
+  triangle.style.height = "100px";
+  triangle.style.rotate = "45deg";
+
+  triangle.style.backgroundColor = "red";
+  triangle.style.position = "absolute";
+  triangle.style.top = spaceToMove+"px";
+  triangle.style.left =  spaceToMove +"px";
+
+  const currentTriangle= document.getElementById("div");
+  document.body.insertBefore(triangle, currentTriangle);
+
+  const circle1 = document.createElement("div");
   
+  circle1.style.width = "100px";
+  circle1.style.height = "100px";
+  circle1.style.borderRadius = "50%";
+
+  circle1.style.backgroundColor = "red";
+  circle1.style.position = "absolute";
+  circle1.style.top = spaceToMove-30+"px";
+  circle1.style.left =  spaceToMove-30+"px";
+
+  const currentcircle1 = document.getElementById("div");
+  document.body.insertBefore(circle1, currentcircle1);
+
+
+  const circle2 = document.createElement("div");
+  
+  circle2.style.width = "100px";
+  circle2.style.height = "100px";
+  circle2.style.borderRadius = "50%";
+
+  circle2.style.backgroundColor = "red";
+  circle2.style.position = "absolute";
+  circle2.style.top = spaceToMove-30+"px";
+  circle2.style.left =  spaceToMove+30+"px";
+
+  const currentcircle2 = document.getElementById("div");
+  document.body.insertBefore(circle2, currentcircle2);
+
+
+
+
 }
